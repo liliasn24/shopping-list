c7d729a426e79f925d6c0d970b62275924c45676
= <salaslilia00@gmail.com>
2021-12-16 23:50:19 -0500

    added provisional schema

--- models/list.js
+++ models/list.js
@@ -1,5 +1,14 @@
 const { Schema, model } = require('mongose');
 
-const listSchema = new Schema({
-    
-})
+const listSchema = new Schema(
+	{
+		store: { type: String, required: true, unique: true },
+		item: String,
+		quantity: String
+	},
+	{
+		timestamps: true
+	}
+);
+
+module.exports = model('List', listSchema);
